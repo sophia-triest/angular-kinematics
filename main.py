b0bdcf0828a74b2e697ed93bed050197433fcbf0
Sophia Triest <s.triest@protonmail.com>
2023-07-16 21:07:57 +0100

    Initial commit
    Version 1

---
+++ main.py
@@ -0,0 +1,97 @@
+# STEP 1: Import the necessary modules.
+import mediapipe as mp
+import cv2
+from mediapipe.python.solutions.drawing_utils import draw_landmarks
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+import math
+from mediapipe import solutions
+from mediapipe.framework.formats import landmark_pb2
+import numpy as np
+
+MARGIN = 10  # pixels
+FONT_SIZE = 1
+FONT_THICKNESS = 1
+HANDEDNESS_TEXT_COLOR = (88, 205, 54) # vibrant green
+
+def getAngle(a, b, c):
+  a = np.array(a)
+  b = np.array(b)
+  c = np.array(c)
+
+  ba = a - b
+  bc = c - b
+
+  cosine_angle = np.dot(ba, bc) / (np.linalg.norm(ba) * np.linalg.norm(bc))
+  angle = np.arccos(cosine_angle)
+
+  return np.degrees(angle)
+
+def draw_landmarks_on_image(rgb_image, detection_result):
+  hand_landmarks_list = detection_result.hand_landmarks
+  handedness_list = detection_result.handedness
+  annotated_image = np.copy(rgb_image)
+
+  # Loop through the detected hands to visualize.
+  for idx in range(len(hand_landmarks_list)):
+    hand_landmarks = hand_landmarks_list[idx]
+    handedness = handedness_list[idx]
+
+    # Draw the hand landmarks.
+    hand_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
+    hand_landmarks_proto.landmark.extend([
+      landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z) for landmark in hand_landmarks
+    ])
+    solutions.drawing_utils.draw_landmarks(
+      annotated_image,
+      hand_landmarks_proto,
+      solutions.hands.HAND_CONNECTIONS,
+      solutions.drawing_styles.get_default_hand_landmarks_style(),
+      solutions.drawing_styles.get_default_hand_connections_style())
+
+    # Get the top left corner of the detected hand's bounding box.
+    height, width, _ = annotated_image.shape
+    x_coordinates = [landmark.x for landmark in hand_landmarks]
+    y_coordinates = [landmark.y for landmark in hand_landmarks]
+    text_x = int(min(x_coordinates) * width)
+    text_y = int(min(y_coordinates) * height) - MARGIN
+
+    # Draw handedness (left or right hand) on the image.
+    cv2.putText(annotated_image, f"{handedness[0].category_name}",
+                (text_x, text_y), cv2.FONT_HERSHEY_DUPLEX,
+                FONT_SIZE, HANDEDNESS_TEXT_COLOR, FONT_THICKNESS, cv2.LINE_AA)
+
+  return annotated_image
+
+# STEP 2: Create an HandLandmarker object.
+base_options = python.BaseOptions(model_asset_path='hand_landmarker.task')
+options = vision.HandLandmarkerOptions(base_options=base_options,
+                                       num_hands=2)
+detector = vision.HandLandmarker.create_from_options(options)
+
+# STEP 3: Load the input image.
+image = mp.Image.create_from_file("handimage2.png")
+
+# STEP 4: Detect hand landmarks from the input image.
+detection_result = detector.detect(image)
+
+
+
+for i in range(19):
+  angle1 = detection_result.hand_landmarks[0][i]
+  angle2 = detection_result.hand_landmarks[0][i+1]
+  angle3 = detection_result.hand_landmarks[0][i+2]
+
+  angle_in_degrees = getAngle(
+    [angle1.x, angle1.y, angle1.z],
+    [angle2.x, angle2.y, angle2.z],
+    [angle3.x, angle3.y, angle3.z]
+
+  )
+  print(str(i) + " " + str(angle_in_degrees))
+
+# STEP 5: Process the classification result. In this case, visualize it.
+annotated_image = draw_landmarks_on_image(image.numpy_view(), detection_result)
+cv2.imshow("landabc", cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
+cv2.waitKey(0)
+cv2.destroyAllWindows()
